dad5a435d87bcb9c42b5be9e353199513b81b08e
AAB <ours.tablet@gmail.com>
2024-01-29 18:05:47 +0000

    Change.

--- Python Stuff/Sequences.py
+++ Python Stuff/Sequences.py
@@ -1,4 +1,4 @@
 string = "String"
-Dictionary = [string[0], "\n", string[1], "\n", string[2], "\n", string[3], "\n", string[4], "\n", string[5], "\n"]
 
-print(Dictionary)
+for i in string:
+    print(string[i + 1])
