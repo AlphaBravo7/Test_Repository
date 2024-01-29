1ccdb182776071b63eef9759465f7aecf2adb18c
AAB <ours.tablet@gmail.com>
2024-01-29 17:42:08 +0000

    Added more "branch" paths.

--- Python Stuff/Conditions.py
+++ Python Stuff/Conditions.py
@@ -4,6 +4,8 @@ if N > 0:
     print(f"The number inputed ({N}) is positive.")
 elif N < 0:
     print(f"The number inputed ({N}) is negative.")
+elif N == 0:
+    print("The number equals 0.")
 else:
     print("Invalid input. Program shutting down.")
     quit()
