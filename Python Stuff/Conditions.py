b6111949d9ba664b02ce56de1352e27cb71f4a73
AAB <ours.tablet@gmail.com>
2024-01-29 17:45:54 +0000

    Bug fix.

--- Python Stuff/Conditions.py
+++ Python Stuff/Conditions.py
@@ -1,12 +1,12 @@
 N = input("Choose A Number: ")
 N = int(N)
-
-if N > 0:
-    print(f"The number inputed ({N}) is positive.")
-elif N < 0:
-    print(f"The number inputed ({N}) is negative.")
-elif N == 0:
-    print("The number equals 0.")
-else:
+try:
+    if N > 0:
+        print(f"The number inputed ({N}) is positive.")
+    elif N < 0:
+        print(f"The number inputed ({N}) is negative.")
+    elif N == 0:
+        print("The number equals 0.")
+except:
     print("Invalid input. Program shutting down.")
     quit()
