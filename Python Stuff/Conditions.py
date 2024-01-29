33834c3b48d422277c62f87f3baf72c71a9a8187
AAB <ours.tablet@gmail.com>
2024-01-29 17:40:43 +0000

    Added a catchall statement.

--- Python Stuff/Conditions.py
+++ Python Stuff/Conditions.py
@@ -2,5 +2,8 @@ N = input("Choose A Number: ")
 
 if N > 0:
     print(f"The number inputed ({N}) is positive.")
-else:
+elif N < 0:
     print(f"The number inputed ({N}) is negative.")
+else:
+    print("Invalid input. Program shutting down.")
+    quit()
