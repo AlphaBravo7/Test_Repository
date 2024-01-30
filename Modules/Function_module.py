8f0fb9c613db13a334f46670fd81762a10bf5215
AAB <ours.tablet@gmail.com>
2024-01-30 18:38:03 +0000

    Change.

--- Modules/Function_module.py
+++ Modules/Function_module.py
@@ -1,16 +1,5 @@
-number = int(input("Please input a number: "))
-sqr_cube = input("Should the number be squared or cubed? ").lower()
+N = int(input("Number: "))
+for i in N:
+    print(N * N)
 
-if sqr_cube == "squared":
-    def Square_number():
-        Sqrd_number = number * number
-        print(f"'{number}' squared is equal to '{Sqrd_number}'!")
-    Square_number()
-    quit()
-
-elif sqr_cube == "cubed":
-    def Cubed_num():
-        Cubed_number = number * number
-        print(f"'{number}' squared is equal to '{Cubed_number}'!")
-    Cubed_num()
-    quit()
+print("Done.")
--- Python_Stuff/ImportedFunction.py
+++ Python_Stuff/ImportedFunction.py
@@ -1,2 +1,4 @@
 from Modules import Function_module
 
+
+
