6345b94ab331ea6052eb95aeb9dd1623ce8a7322
AAB <ours.tablet@gmail.com>
2024-01-30 18:15:40 +0000

    First commit.

---
+++ Python Stuff/Dictionaries.py
@@ -0,0 +1,4 @@
+key_value = {"Docter":  "Definition: A medical professional.",
+             "Officer":  "Definition: Someone who serves for the public and protects the public as well."}
+
+print(key_value["Docter"])
