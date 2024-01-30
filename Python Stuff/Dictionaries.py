4a6a4e5e933813a658ac552fda0b30adeb182fc9
AAB <ours.tablet@gmail.com>
2024-01-30 18:17:35 +0000

    Second commit.

--- Python Stuff/Dictionaries.py
+++ Python Stuff/Dictionaries.py
@@ -1,4 +1,6 @@
 key_value = {"Docter":  "Definition: A medical professional.",
              "Officer":  "Definition: Someone who serves for the public and protects the public as well."}
 
+key_value["Farmer"] = "Definition: A person who cultivates fruits, vegetables and possibly animals to be consumed."
+
 print(key_value["Docter"])
