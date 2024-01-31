d9326fa9aef3c41c82c9e058573323a339e8facf
AAB <ours.tablet@gmail.com>
2024-01-31 16:52:20 +0000

    Revamp.

--- Python_Stuff/Classes.py
+++ Python_Stuff/Classes.py
@@ -1,9 +1,23 @@
-class Point():
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+class Flight():
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.passengers = []
 
-p = Point(10, 2)
+    def add_passenger(self, name):
+        if not self.open_seats():
+            return False
+        self.passengers.append(name)
+        return True
+    
+    def open_seats(self):
+        return self.capacity - len(self.passengers)
+        
+flight = Flight(3)
 
-print(p.x)
-print(p.y)
+people = ["Harry", "Ron", "Jake", "Tom"]
+for person in people:
+    success = flight.add_passenger(person)
+    if success:
+        print(f"Added {person} to flight successfully!")
+    else:
+        print(f"No available seats for: {person}")
