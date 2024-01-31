7bf19d4317e68d4a913823b976903640dc7574cf
AAB <ours.tablet@gmail.com>
2024-01-31 17:15:07 +0000

    First commit.

---
+++ Python_Stuff/Exceptions.py
@@ -0,0 +1,13 @@
+import sys
+
+x = int(input("X: "))
+y = int(input("Y: "))
+
+try:
+    result = x / y
+except ZeroDivisionError:
+    print(f"ERROR: You cannot divide by 0!")
+    sys.exit(1)
+
+print(f"{x} / {y} = {result}")
+
