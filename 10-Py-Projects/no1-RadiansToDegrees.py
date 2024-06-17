e95975211b63fff36a57b5abe9241c23f23b7445
AAB <ours.tablet@gmail.com>
2024-06-17 15:18:21 +0100

    new.

---
+++ 10-Py-Projects/no1-RadiansToDegrees.py
@@ -0,0 +1,17 @@
+# 1 degree = 57.2957795 radians
+
+# Degrees to Radians
+
+degrees = 150
+
+radians = degrees * 57.2957795
+
+print(f"{degrees} radians is {radians} degrees.")
+
+
+
+# Radians to Degrees
+
+radians = degrees / 57.2957795
+
+print(f"{radians} radians is {degrees} degrees.")
